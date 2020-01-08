beb866a23b15f85882646cdc8d31969cd290920a
/game <VigilanteToolset@gmail.com>
2020-01-08 03:12:38 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
-#!/bin/bash/
 apt-get update
 apt-get install terminator -y
 apt-get install nikto -y
-apt-get install python3 -y  
+apt-get install python3 -y
 apt-get install python3-pip -y
 apt-get install sqlmap -y
 apt-get install uniscan -y
@@ -13,6 +12,14 @@ apt-get install nmap -y
 apt-get install torbrowser-launcher -y
 apt-get install etherape -y
 gcc ddos.c -o ddos
-chmod +x ./
-
-
+gcc cDDoS.c -o cDDoS
+chmod +x bruTOR
+chmod +x bruTOR.sh
+chmod +x ddos.sh
+chmod +x ddos
+chmod +x cDDoS
+chmod +x ddos8000.sh
+chmod +x ddosx4.sh
+chmod +x proxy.sh
+chmod +x sql.sh
+chmod +x Vigilante
